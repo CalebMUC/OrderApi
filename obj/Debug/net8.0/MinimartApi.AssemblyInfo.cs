7a31b97ddbb1f7db723ad957fd0a72bb7d9120bf
calebMUC <muchiricaleb05@gmail.com>
2025-05-31 12:20:13 +0300

    Enviroment Variables RabbitMQ Redis

--- obj/Debug/net8.0/MinimartApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimartApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a04f7be21101a5cd0b156c006cfb8feffd23192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7665aa98438e893ec707cf6cea5ec0733c88b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
