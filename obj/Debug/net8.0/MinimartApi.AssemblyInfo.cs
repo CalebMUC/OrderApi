0d58b9d97e639ce7471fa28b2ebbd6ef05f906b4
calebMUC <muchiricaleb05@gmail.com>
2025-10-04 17:54:59 +0300

    Rebase

--- obj/Debug/net8.0/MinimartApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimartApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722cc6e2cf1df0a6f5d4e58e971ef8ceb38775a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0041671c38d4b29ac61a9ce07955f6ba498281")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
