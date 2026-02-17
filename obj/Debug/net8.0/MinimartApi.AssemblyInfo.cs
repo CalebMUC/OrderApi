a7c74a54813280ba25527a1d85ddc17a6d889462
calebMUC <muchiricaleb05@gmail.com>
2026-02-17 10:44:08 +0300

    Social Medial Crawler Server side rendered html

--- obj/Debug/net8.0/MinimartApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimartApi.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b4ca70744653757fee0917b7a5e54ebb7ed52c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd02299ca0d16213f6c9d63f93adc39899fef3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimartApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
